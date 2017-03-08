e50774c67524e2f2ad92121b99ccb35d1e9a0fe7
Nathael Arki <nathael.arki@supinternet.fr>
2017-03-08 13:39:40 +0100

    image txt

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,4 +4,24 @@ window.onload = function (){
         document.querySelector('#create_folder').style.display = 'block';
         document.querySelector('#new_folder').style.display = "none";
     }
+
+    var close = document.querySelectorAll('.close');
+    
+    for(var i = 0; i < close.length; i++){
+		close[i].onclick = function(){
+		this.parentNode.style.display = "none";
+        this.parentNode.parentNode.childNodes[2].style.display = "none";
+        this.parentNode.parentNode.childNodes[0].style.display = "inline";
+		}
+	}
+
+    var open = document.querySelectorAll('.open');
+
+    for(var i = 0; i < open.length; i++){
+		open[i].onclick = function(){
+		this.parentNode.style.display = "none";
+        this.parentNode.parentNode.childNodes[1].style.display = "block";
+        this.parentNode.parentNode.childNodes[2].style.display = "flex";
+		}
+	}
 }
